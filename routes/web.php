d88f3969d578321124da03466c47724cb1237f2f
MalikAbdullah1318 <malikabdullah1318@gmail.com>
2026-08-11 03:36:38 -0700

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use MalikAbdullah1318\LaravelInstaller\Installer;
 
-Route::middleware(function ($request, $next) {
-
-    if (app(Installer::class)->isInstalled()) {
-        abort(404);
-    }
-
-    return $next($request);
-
-})->prefix(config('installer.route'))->group(function () {
+Route::prefix(config('installer.route'))->group(function () {
 
     Route::get('/', function () {
+
+        if (app(Installer::class)->isInstalled()) {
+            abort(404);
+        }
+
         return view('installer::installer.welcome');
+
     })->name('installer.welcome');
 
 });
