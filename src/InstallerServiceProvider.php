8b49063686ab5ca0f8f9230d34d3a278e88f7513
MalikAbdullah1318 <malikabdullah1318@gmail.com>
2026-08-13 16:44:47 +0500

    updated service provider

--- src/InstallerServiceProvider.php
+++ src/InstallerServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace MalikAbdullah1318\LaravelInstaller;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use MalikAbdullah1318\LaravelInstaller\Http\Middleware\InstallerNotInstalled;
 use MalikAbdullah1318\LaravelInstaller\Environment\EnvironmentManager;
@@ -30,18 +30,32 @@ class InstallerServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-
         /*
         |--------------------------------------------------------------------------
         | Installer Session
         |--------------------------------------------------------------------------
-        |
-        | The installer must work before the application's database
-        | and .env file are configured. Therefore use file sessions.
-        |
         */
 
         Config::set('session.driver', 'file');
+
+        /*
+        |--------------------------------------------------------------------------
+        | Temporary Application Key
+        |--------------------------------------------------------------------------
+        |
+        | The installer runs before the application's .env exists.
+        | Laravel's cookie/session system still requires an encryption key.
+        |
+        */
+
+        if (! config('app.key')) {
+            Config::set(
+                'app.key',
+                'base64:' . base64_encode(
+                    random_bytes(32)
+                )
+            );
+        }
 
         $this->publishes([
             __DIR__ . '/../config/installer.php' =>
